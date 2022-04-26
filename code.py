5ef666d7a4044e46373ea3169b7ce04d47070e0a
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-04-26 17:44:05 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 ## you data need to be a list with decimal values (bytes converted in decimal)
 
-## EX:   data = [0xFA, 0x6D, 0xB6] you data need is -> data = [250, 109, 182]
+## EX:   data = [0xFA, 0x6D, 0xB6] you data need be -> data = [250, 109, 182]
 
 ## this code use a poly = 0x1021, you can change for you poly
 
